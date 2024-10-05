b9de1843b71b47268514ecaa4ebf52e4d0db021b
sudho712 <sudho712@gmail.com>
2024-10-05 11:37:23 +0530

    navbar create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
+let name="raj";
 function App() {
   return (
     <>
     
-   <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact</li>
-    
-   </nav>
-   <div className="container">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos suscipit adipisci molestias nisi aspernatur recusandae iste, eveniet illo ea. Consectetur obcaecati quae nobis doloremque adipisci doloribus tenetur reprehenderit maxime.
-    Deserunt molestias sit ea hic incidunt maiores sunt neque accusamus adipisci dolor error magnam ratione quo architecto blanditiis mollitia voluptatum repellendus reiciendis voluptate, esse nam repellat? Quaerat aliquid delectus rerum?
-    Commodi dolor in rerum ipsa consequatur placeat amet quos. Laborum inventore blanditiis sapiente, aliquid suscipit eaque odio consequuntur quod aliquam, voluptatibus repellat ipsa odit culpa quaerat ex est at maxime.
-    Ullam, impedit. Quo, odit hic! Perferendis nisi molestias aliquam quos, nostrum similique sed ut tempora autem non in at architecto exercitationem dolore esse dicta dolor magnam rem! Facilis, molestias accusantium.
-    Dolorum commodi sapiente illum, pariatur aperiam voluptatum doloremque possimus deserunt itaque. Necessitatibus facere quos consequuntur laudantium in dicta corporis? Repellendus recusandae temporibus doloribus voluptates quasi pariatur molestiae tenetur, eos veritatis!</p>
-   </div>
+<Navbar></Navbar>
     
     
     </>
