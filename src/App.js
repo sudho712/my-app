4fd168826c48018978c0c81a895b52446e31568b
sudho712 <sudho712@gmail.com>
2024-10-05 10:35:36 +0530

    sucessfully run

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Leran react testing
-        </a>
-      </header>
-    </div>
+    <>
+    
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+    
+   </nav>
+   <div className="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos suscipit adipisci molestias nisi aspernatur recusandae iste, eveniet illo ea. Consectetur obcaecati quae nobis doloremque adipisci doloribus tenetur reprehenderit maxime.
+    Deserunt molestias sit ea hic incidunt maiores sunt neque accusamus adipisci dolor error magnam ratione quo architecto blanditiis mollitia voluptatum repellendus reiciendis voluptate, esse nam repellat? Quaerat aliquid delectus rerum?
+    Commodi dolor in rerum ipsa consequatur placeat amet quos. Laborum inventore blanditiis sapiente, aliquid suscipit eaque odio consequuntur quod aliquam, voluptatibus repellat ipsa odit culpa quaerat ex est at maxime.
+    Ullam, impedit. Quo, odit hic! Perferendis nisi molestias aliquam quos, nostrum similique sed ut tempora autem non in at architecto exercitationem dolore esse dicta dolor magnam rem! Facilis, molestias accusantium.
+    Dolorum commodi sapiente illum, pariatur aperiam voluptatum doloremque possimus deserunt itaque. Necessitatibus facere quos consequuntur laudantium in dicta corporis? Repellendus recusandae temporibus doloribus voluptates quasi pariatur molestiae tenetur, eos veritatis!</p>
+   </div>
+    
+    
+    </>
   );
 }
 
